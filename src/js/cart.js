69c0ee4a672fb95fa8124da1956435ffa16b6a7e
liudiming <1378542732@qq.com>
2020-07-11 11:36:38 +0800

    0.0.27
    增加购物车渲染功能

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,11 +1,56 @@
-$(".table-add").on("click",function(){
-    console.log($(".num").val())
-    var num = parseInt($(".num").val())
-    $(".num").val(num+1)
-    var num1 = parseInt($(".num").val())
-    var price1 = parseInt($('.productPrice').text().slice(2,6))
-    var sum = num1 * price1
-    $(".sum").text("￥"+sum)
+$.ajax({
+    url:'../php/showlist.php',
+    success:function(data){
+        // console.log(data.data)
+        $("tbody").empty()
+        $.each(data.data,function(index,value){
+            // console.log(value)
+            // console.log(data.data.length)
+            // var shuliang = data.data.length
+            $(".shuliang").text(data.data.length)
+            var price = (value.product_price)
+            price = parseInt(price.slice(1,5))
+            var sum = value.product_num * price
+            // console.log(price)
+            var str = `<tr index="${value.product_id}" class="product${value.product_id}">
+                            <td class="productImage">
+                                <img src="${value.product_img}" alt="">
+                            </td>
+                            <td class="productname">
+                                <a href="" class="pdt-title">${value.product_name}</a>
+                            </td>
+                            <td class="productPrice"> ${value.product_price}</td>
+                            <td>
+                                <div class="btn-cnts">
+                                    <span class="table-cut">-</span>
+                                    <input type="text" value="${value.product_num}" class="num cart-cnt" maxlength="2" readonly="readonly">
+                                    <span class="table-add">+</span>
+                                </div>
+                            </td>
+                            <td class="sum" style="line-height: 1.8;">
+                                ￥${sum}
+                            </td>
+                            <td>
+                                <a title="删除" href="" class="cart-close close">x</a>
+                            </td>
+                        </tr>`
+
+                $("tbody").append(str)
+        })
+    },
+    dataType:"json"
+})
+
+
+
+// console.log($('.table-add'))
+
+$("tbody").on("click",function(){
+    // console.log($(this).children().length)
+    $(this).children().on("click",function(){
+        console.log($(this).children(".table-add"))
+
+    })
 })
 
 $(".table-cut").on("click",function(){
@@ -22,11 +67,8 @@ $(".table-cut").on("click",function(){
 })
 
 
-$.ajax({
-    url:'../php/showlist.php',
-    success:function(data){
-        console.log(data)
-    },
-    dataType:"json"
-})
+
+
+
+
 
